5094d2cdad1ea8c0131e00f25e1a94a2935f7d41
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-08-14 18:59:08 -0400

    Update book construction

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,12 +1,20 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 class Book extends React.Component {
-     render() {
-        // const { title, author, pages } = this.props.details;
+    render() {
+        const { title, author, desc } = this.props.details;
         return (
-            <p>book</p>
+            <div className="book-grid">
+                <li className="book-list">
+                    <h3 className="book-title">{title}</h3>
+                    <span>{author}</span>
+                    <p>{desc}</p>
+                    <button className="book-button">Add to Library</button>
+                </li>
+            </div>
         )
     }
-};
+}
 
 export default Book;
