d3b7c17ee5db603646ec90804ff110526954a115
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-08-15 19:00:37 -0400

    add rendering for books added to the library. allow for books to be removed from the library

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,13 +1,39 @@
 import React from "react";
 
 class Library extends React.Component {
-    render() {
+    renderLibrary = key => {
+        const book = this.props.books[key];
+        const isAvailable = book.status === "available";
+        if (!isAvailable) {
+          return (
+            <li key={key}>
+              Sorry {book ? book.title : "book"} is no longer available
+            </li>
+          );
+        }
         return (
-            <div className="library">
-                <h2>Your Library</h2>
+          <li key={key}>
+            <h3>{book.title}</h3>
+            <h4>{book.author}</h4>
+            <button onClick={() => this.props.expandBookDetails(key)}>Expand Details</button>
+            <button onClick={() => this.props.removeFromLibrary(key)}>
+                  &times; Remove
+            </button>
+          </li>
+        );
+      };
+      render() {
+        const libraryIds = Object.keys(this.props.library);
+        
+        return (
+          <div className="library-wrap">
+            <div className="library-header">
+            <h2>My Library</h2>
             </div>
-        )
+            <ul className="library">{libraryIds.map(this.renderLibrary)}</ul>
+          </div>
+        );
+      }
     }
-}
-
+    
 export default Library;
