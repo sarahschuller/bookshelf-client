c32f5ed660f8203422498a958fbded1bd0918756
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-08-15 18:58:31 -0400

    create sample-books.js

---
+++ src/sample-books.js
@@ -0,0 +1,75 @@
+const books = {
+
+    book1: {
+      title: "The Lord of the Rings",
+      author: "J.R.R. Tolkien",
+      desc:
+        "The Lord of the Rings is an epic high-fantasy novel by English author and scholar J. R. R. Tolkien. Set in Middle-earth, intended to be Earth at some time in the distant past, the story began as a sequel to Tolkien's 1937 children's book The Hobbit, but eventually developed into a much larger work.",
+      genre: "High Fantasy",
+      status: "available",
+    },
+  
+    book2: {
+      title: "Mistborn: The Final Empire",
+      author: "Brandon Sanderson",
+      desc:
+        "The Final Empire, the first book in a trilogy by Brandon Sanderson, is a tale of a subjugated people known as the Skaa, and their fight for freedom against a seemingly invincible God known as the Lord Ruler. The Lord Ruler has ruled this world for a thousand years through his Inquisitors and Obligators.",
+      genre: "Fantasy",
+      status: "available",
+    },
+  
+    book3: {
+      title: "The Eye of the World",
+      author: "Robert Jordan",
+      desc:
+        "The Wheel of Time turns and Ages come and pass, leaving memories that become legend. Legend fades to myth, and even myth is long forgotten when the Age that gave it birth returns again. What was, what will be, and what is, may yet fall under the Shadow.",
+      genre: "High Fantasy",
+      status: "available",
+    },
+  
+    book4: {
+      title: "The Stand",
+      author: "Stephen King",
+      desc:
+        "The Stand is a post-apocalyptic dark fantasy novel written by American author Stephen King and first published in 1978 by Doubleday.",
+      genre: "Dark Fantasy",
+      status: "available",
+    },
+  
+    book5: {
+      title: "Harry Potter and the Sorcerer's Stone",
+      author: "J.K. Rowling",
+      desc:
+        "Harry Potter has no idea how famous he is. That's because he's being raised by his miserable aunt and uncle who are terrified Harry will learn that he's really a wizard, just as his parents were. But everything changes when Harry is summoned to attend an infamous school for wizards, and he begins to discover some clues about his illustrious birthright. From the surprising way he is greeted by a lovable giant, to the unique curriculum and colorful faculty at his unusual school, Harry finds himself drawn deep inside a mystical world he never knew existed and closer to his own noble destiny. these open and enjoy them plain or with one of our cocktail sauces",
+      genre: "Fantasy",
+      status: "available",
+    },
+  
+    book6: {
+      title: "Carrie",
+      author: "Stephen King",
+      desc: " Unpopular at school and subjected to her mother's religious fanaticism at home, Carrie White does not have it easy. But while she may be picked on by her classmates, she has a gift she's kept secret since she was a little girl: she can move things with her mind. Doors lock. Candles fall. Her ability has been both a power and a problem. And when she finds herself the recipient of a sudden act of kindness, Carrie feels like she's finally been given a chance to be normal. She hopes that the nightmare of her classmates' vicious taunts is over . . . but an unexpected and cruel prank turns her gift into a weapon of horror so destructive that the town may never recover. ",
+      genre: "Horor",
+      status: "available",
+    },
+  
+    book7: {
+      title: "Haunted",
+      author: "Chuck Palahniuk",
+      desc:
+        "Haunted is a novel made up of twenty-three horrifying, hilarious, and stomach-churning stories. They’re told by people who have answered an ad for a writer’s retreat and unwittingly joined a “Survivor”-like scenario where the host withholds heat, power, and food. As the storytellers grow more desperate, their tales become more extreme, and they ruthlessly plot to make themselves the hero of the reality show that will surely be made from their plight. This is one of the most disturbing and outrageous books you’ll ever read, one that could only come from the mind of Chuck Palahniuk. ",
+      genre: "Dark Comedy",
+      status: "available",
+    },
+  
+    book8: {
+      title: "One Second After",
+      author: "William R. Forstchen",
+      desc:
+        "New York Times best selling author William R. Forstchen now brings us a story which can be all too terrifyingly real...a story in which one man struggles to save his family and his small North Carolina town after America loses a war, in one second, a war that will send America back to the Dark Ages...A war based upon a weapon, an Electro Magnetic Pulse (EMP). A weapon that may already be in the hands of our enemies.",
+      genre: "Post Apocalyptic",
+      status: "available",
+    }
+  };
+  
+  export default books;
