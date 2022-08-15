2dfcaebf4783e83fda4f6b9fb0aab96ccbb62705
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-08-15 19:00:12 -0400

    update to include status

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,14 +2,19 @@ import React from "react";
 
 class Book extends React.Component {
     render() {
-        const { title, author, desc } = this.props.details;
+        const { title, author, desc, status } = this.props.details;
+        const isAvailable = status === "available";
         return (
             <div className="book">
                 <li className="book-list">
                     <h3 className="book-title">{title}</h3>
-                    <span>{author}</span>
+                    <span className="author-name">{author}</span>
                     <p>{desc}</p>
-                    <button className="book-button">Add to Library</button>
+                    <button 
+                    disabled={!isAvailable}
+                    className="book-button"
+                    onClick={() => this.props.addToLibrary(this.props.index)}
+                    >{isAvailable ? "Add to Library" : "Book Unavailable"}</button>
                 </li>
             </div>
         )
