2b67746eba425d254be62010db8ca0364c66abc9
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-08-27 11:35:56 -0400

    create BookDetails module to contain code for modal. add .js to editbookform name

---
+++ src/components/BookDetails.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class BookDetails extends React.Component {
+    render() {
+        return (
+            <div className="modal-outer">
+                <div className="modal-inner">
+                    <h2>The Shelf</h2>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default BookDetails;
